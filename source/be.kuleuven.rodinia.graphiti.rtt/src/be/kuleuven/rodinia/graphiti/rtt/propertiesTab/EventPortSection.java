02388d8e121239bfc76d4c36232bb435718d46d9
Ego <ego@ubuntu.(none)>
2013-08-28 09:55:16 +0200

    some files were missing

---
+++ source/be.kuleuven.rodinia.graphiti.rtt/src/be/kuleuven/rodinia/graphiti/rtt/propertiesTab/EventPortSection.java
@@ -0,0 +1,8 @@
+package be.kuleuven.rodinia.graphiti.rtt.propertiesTab;
+
+import org.eclipse.graphiti.ui.platform.GFPropertySection;
+import org.eclipse.ui.views.properties.tabbed.ITabbedPropertyConstants;
+
+public class EventPortSection extends GFPropertySection implements ITabbedPropertyConstants{
+
+}
