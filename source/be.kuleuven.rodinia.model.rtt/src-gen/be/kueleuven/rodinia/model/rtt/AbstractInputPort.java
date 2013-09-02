df8c24fcbb64f089dc4e75259b7c5e4751125ff7
Hugo A. Garcia <hugo.garcia@mech.kuleuven.be>
2013-09-02 15:17:47 +0200

    Added AbstractImputPort to RTT model.

---
+++ source/be.kuleuven.rodinia.model.rtt/src-gen/be/kueleuven/rodinia/model/rtt/AbstractInputPort.java
@@ -0,0 +1,80 @@
+/**
+ */
+package be.kueleuven.rodinia.model.rtt;
+
+import be.kuleuven.rodinia.dsl.datatypes.datatypes.DataType;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Abstract Input Port</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link be.kueleuven.rodinia.model.rtt.AbstractInputPort#getName <em>Name</em>}</li>
+ *   <li>{@link be.kueleuven.rodinia.model.rtt.AbstractInputPort#getDataType <em>Data Type</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see be.kueleuven.rodinia.model.rtt.RttPackage#getAbstractInputPort()
+ * @model abstract="true"
+ * @generated
+ */
+public interface AbstractInputPort extends EObject
+{
+  /**
+   * Returns the value of the '<em><b>Name</b></em>' attribute.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Name</em>' attribute isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Name</em>' attribute.
+   * @see #setName(String)
+   * @see be.kueleuven.rodinia.model.rtt.RttPackage#getAbstractInputPort_Name()
+   * @model unique="false" required="true"
+   * @generated
+   */
+  String getName();
+
+  /**
+   * Sets the value of the '{@link be.kueleuven.rodinia.model.rtt.AbstractInputPort#getName <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Name</em>' attribute.
+   * @see #getName()
+   * @generated
+   */
+  void setName(String value);
+
+  /**
+   * Returns the value of the '<em><b>Data Type</b></em>' reference.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Data Type</em>' reference isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Data Type</em>' reference.
+   * @see #setDataType(DataType)
+   * @see be.kueleuven.rodinia.model.rtt.RttPackage#getAbstractInputPort_DataType()
+   * @model
+   * @generated
+   */
+  DataType getDataType();
+
+  /**
+   * Sets the value of the '{@link be.kueleuven.rodinia.model.rtt.AbstractInputPort#getDataType <em>Data Type</em>}' reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Data Type</em>' reference.
+   * @see #getDataType()
+   * @generated
+   */
+  void setDataType(DataType value);
+
+} // AbstractInputPort
---
+++ source/be.kuleuven.rodinia.model.rtt/src-gen/be/kueleuven/rodinia/model/rtt/impl/AbstractInputPortImpl.java
@@ -0,0 +1,245 @@
+/**
+ */
+package be.kueleuven.rodinia.model.rtt.impl;
+
+import be.kueleuven.rodinia.model.rtt.AbstractInputPort;
+import be.kueleuven.rodinia.model.rtt.RttPackage;
+
+import be.kuleuven.rodinia.dsl.datatypes.datatypes.DataType;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Abstract Input Port</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link be.kueleuven.rodinia.model.rtt.impl.AbstractInputPortImpl#getName <em>Name</em>}</li>
+ *   <li>{@link be.kueleuven.rodinia.model.rtt.impl.AbstractInputPortImpl#getDataType <em>Data Type</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public abstract class AbstractInputPortImpl extends MinimalEObjectImpl.Container implements AbstractInputPort
+{
+  /**
+   * The default value of the '{@link #getName() <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getName()
+   * @generated
+   * @ordered
+   */
+  protected static final String NAME_EDEFAULT = null;
+
+  /**
+   * The cached value of the '{@link #getName() <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getName()
+   * @generated
+   * @ordered
+   */
+  protected String name = NAME_EDEFAULT;
+
+  /**
+   * The cached value of the '{@link #getDataType() <em>Data Type</em>}' reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getDataType()
+   * @generated
+   * @ordered
+   */
+  protected DataType dataType;
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected AbstractInputPortImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return RttPackage.Literals.ABSTRACT_INPUT_PORT;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public String getName()
+  {
+    return name;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setName(String newName)
+  {
+    String oldName = name;
+    name = newName;
+    if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, RttPackage.ABSTRACT_INPUT_PORT__NAME, oldName, name));
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public DataType getDataType()
+  {
+    if (dataType != null && dataType.eIsProxy())
+    {
+      InternalEObject oldDataType = (InternalEObject)dataType;
+      dataType = (DataType)eResolveProxy(oldDataType);
+      if (dataType != oldDataType)
+      {
+        if (eNotificationRequired())
+          eNotify(new ENotificationImpl(this, Notification.RESOLVE, RttPackage.ABSTRACT_INPUT_PORT__DATA_TYPE, oldDataType, dataType));
+      }
+    }
+    return dataType;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public DataType basicGetDataType()
+  {
+    return dataType;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setDataType(DataType newDataType)
+  {
+    DataType oldDataType = dataType;
+    dataType = newDataType;
+    if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, RttPackage.ABSTRACT_INPUT_PORT__DATA_TYPE, oldDataType, dataType));
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public Object eGet(int featureID, boolean resolve, boolean coreType)
+  {
+    switch (featureID)
+    {
+      case RttPackage.ABSTRACT_INPUT_PORT__NAME:
+        return getName();
+      case RttPackage.ABSTRACT_INPUT_PORT__DATA_TYPE:
+        if (resolve) return getDataType();
+        return basicGetDataType();
+    }
+    return super.eGet(featureID, resolve, coreType);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public void eSet(int featureID, Object newValue)
+  {
+    switch (featureID)
+    {
+      case RttPackage.ABSTRACT_INPUT_PORT__NAME:
+        setName((String)newValue);
+        return;
+      case RttPackage.ABSTRACT_INPUT_PORT__DATA_TYPE:
+        setDataType((DataType)newValue);
+        return;
+    }
+    super.eSet(featureID, newValue);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public void eUnset(int featureID)
+  {
+    switch (featureID)
+    {
+      case RttPackage.ABSTRACT_INPUT_PORT__NAME:
+        setName(NAME_EDEFAULT);
+        return;
+      case RttPackage.ABSTRACT_INPUT_PORT__DATA_TYPE:
+        setDataType((DataType)null);
+        return;
+    }
+    super.eUnset(featureID);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public boolean eIsSet(int featureID)
+  {
+    switch (featureID)
+    {
+      case RttPackage.ABSTRACT_INPUT_PORT__NAME:
+        return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
+      case RttPackage.ABSTRACT_INPUT_PORT__DATA_TYPE:
+        return dataType != null;
+    }
+    return super.eIsSet(featureID);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public String toString()
+  {
+    if (eIsProxy()) return super.toString();
+
+    StringBuffer result = new StringBuffer(super.toString());
+    result.append(" (name: ");
+    result.append(name);
+    result.append(')');
+    return result.toString();
+  }
+
+} //AbstractInputPortImpl
